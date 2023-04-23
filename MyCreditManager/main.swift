b896dd087d02bee734097ad2a3be784172f2832f
donghyun1998 <donghyun1998@naver.com>
2023-04-23 20:42:45 +0900

    클래스 공부 필요

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -7,18 +7,68 @@
 
 import Foundation
 
+enum   Grade: Float
+{
+    case aplus = 4.5
+    case a     = 4
+    case bplus = 3.5
+    case b     = 3
+    case cplus = 2.5
+    case c     = 2
+    case dplus = 1.5
+    case d     = 1
+    case f     = 0
+}
+
+var Subject : (String, String) = ("dummy", "F")
+
+class Student
+{
+//    let name: String
+//    var grades: String
+    var student_record: Dictionary<String, String> = Dictionary<String, String>()
+
+    
+    init(name: String, grade: String)
+    {
+        student_record[name] = grade
+    }
+}
+
+func    x_cmd()
+{
+    print("프로그램을 종료합니다...")
+    exit(0)
+}
+
+func    add_student(students: Array<Student>)
+{
+    while (true)
+    {
+        let name = readLine()
+        if let str = name
+        {
+        }
+        else
+        {
+            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+            break
+        }
+        if
+        
+    }
+}
+var students: [Student] = []
 while (true)
 {
     print("원하는 기능을 입력해주세요")
     print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
-    let input: String? = readLine()
+    let input = readLine()
     
-    if let input = input
+    switch input
     {
-        switch input
-        {
         case "1":
-            print("학생추가")
+            add_student(students)
         case "2":
             print("학생삭제")
         case "3":
@@ -28,14 +78,8 @@ while (true)
         case "5":
             print("평점보기")
         case "X":
-            exit(0)
+            x_cmd()
         default:
             print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
-        }
-    }
-    else
-    {
-        print("nil")
-        exit(1)
     }
 }
